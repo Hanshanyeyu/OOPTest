6612b35d055bd93fea670eb00148b52d726e2528
pj919715177 <pengjun@ycf.com>
2016-08-31 14:56:32 +0800

    调格式

--- DesignPattern/Single.php
+++ DesignPattern/Single.php
@@ -21,7 +21,7 @@ class Single
     public function update()
     {
         echo '
-        我是单例模式，可以通过Factory实例化对象，
+        我是单例模式，可以通过Factory生产对象，
         因为我没有extra()函数,无法实例化Observerable接口，
         只能通过Adapter，将Adapter实例放在Register的注册树属性数组中，
         将注册树的属性添加到Observer的观察者数组中，当Observer执行change()函数时，
