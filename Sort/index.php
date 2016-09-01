a86167a77da042e4986f67b20c1df5b22074f823
pj919715177 <pengjun@ycf.com>
2016-09-01 18:12:32 +0800

    。。

---
+++ Sort/index.php
@@ -0,0 +1,13 @@
+<?php
+namespace Pj\Sort;
+
+include_once 'Test.php';
+use Pj\Sort\Test;
+
+$class = ['HeapSort', 'InsertionSort', 'MergeSort', 'QuickSort', 'SelectionSort'];
+$prefix = 'Pj\Sort\\';
+$origin = [12,47,6,8];
+$sorted = [6,8,12,47];
+$test = new Test($prefix, $class);
+$test->addTest($origin, $sorted);
+$test->testing();
--- Sort/test.php
+++ Sort/test.php
@@ -1,66 +1,78 @@
 <?php
-
 namespace pj\Sort;
-
 include_once 'HeapSort.php';
 include_once 'InsertionSort.php';
 include_once 'MergeSort.php';
 include_once 'QuickSort.php';
 include_once 'SelectionSort.php';
 
-use pj\Sort\HeapSort;
-use pj\Sort\InsertionSort;
-use pj\Sort\MergeSort;
-use pj\Sort\QuickSort;
-use pj\Sort\SelectionSort;
+class Test
+{
+    private $prefix;
+    private $className = array();
+    private $origin = array();
+    private $sorted = array();
+    private $count = 0;
 
-$class = ['HeapSort', 'InsertionSort', 'MergeSort', 'QuickSort', 'SelectionSort'];
-$origin = [
-    [23, 56, 11, -6, 3, 88, 22, 56, 2, 8],
-    [34, 67, -890, 435, 546, 78, 234, 87432, 125, 634],
-    [23, 54, 76, 897, 234, -2534, 432, 87, 65, 346]
-];
-$sorted = [
-    [-6, 2, 3, 8, 11, 22, 23, 56, 56, 88],
-    [-890, 34, 67, 78, 125, 234, 435, 546, 634, 87432],
-    [-2534, 23, 54, 65, 76, 87, 234, 346, 432, 897]
-];
+    public function __construct($prefix, $className)
+    {
+        $this->prefix = $prefix;
+        if (is_array($className)) {
+            $this->className = $className;
+        } else {
+            $this->className = [$className];
+        }
+        $this->origin = [
+            [23, 56, 11, -6, 3, 88, 22, 56, 2, 8],
+            [34, 67, -890, 435, 546, 78, 234, 87432, 125, 634],
+            [23, 54, 76, 897, 234, -2534, 432, 87, 65, 346]
+        ];
+        $this->sorted = [
+            [-6, 2, 3, 8, 11, 22, 23, 56, 56, 88],
+            [-890, 34, 67, 78, 125, 234, 435, 546, 634, 87432],
+            [-2534, 23, 54, 65, 76, 87, 234, 346, 432, 897]
+        ];
+        $this->count = 3;
+    }
 
-//能跑的
-//$temp = new HeapSort($origin[0]);
+    public function addTest(array $origin, array $sorted)
+    {
+        if (count($origin) === count($sorted)) {
+            $this->origin[] = $origin;
+            $this->sorted[] = $sorted;
+            $this->count++;
+        } else {
+            echo "<script>alert('原数组或排序好的数组错误');</script>";
+        }
+    }
 
-//有问题的
-$param = $class[0];
-$temp = new $param($origin[0]);
+    public function testing()
+    {
+        for ($i = 0; $i < $this->count; $i++) {
+            echo '对于数列';
+            var_dump($this->origin[$i]);
+            echo '<br/>';
+            echo '正确顺序';
+            var_dump($this->sorted[$i]);
+            echo '<br/>';
+            $success = 0;
+            $fail = 0;
+            foreach ($this->className as $class) {
+                echo $class . '排列的结果为: <br/>';
+                $class = $this->prefix . $class;
+                $temp = new $class($this->origin[$i]);
+                $temp->sort();
+                if ($temp->getSortedArray() === $this->sorted[$i]) {
+                    echo '<font style="color:green">成功</font>';
+                    $success++;
+                } else {
+                    echo '<font style="color:red">失败</font>';
+                    $fail++;
+                }
+                echo '<br/>';
+            }
+            echo "排列共成功 $success 次，失败 $fail 次<br/>";
 
-$temp->sort();
-var_dump($temp->getSortedArray());
-
-
-
-//$var = 'pj\Sort';
-//for ($i = 0; $i < 3; $i++) {
-//    echo '对于数列';
-//    var_dump($origin[$i]);
-//    echo '<br/>';
-//    echo '正确顺序';
-//    var_dump($sorted[$i]);
-//    echo '<br/>';
-//    $success = 0;
-//    $fail = 0;
-//    foreach ($class as $clas) {
-//        echo $clas . '排列的结果为: <br/>';
-//        $clas = $var . '\\' . $clas;
-//        $temp = new $clas($origin[$i]);
-//        $temp->sort();
-//        if ($temp->getSortedArray() === $sorted[$i]) {
-//            echo '<font style="color:green">成功</font>';
-//            $success++;
-//        } else {
-//            echo '<font style="color:red">失败</font>';
-//            $fail++;
-//        }
-//        echo '<br/>';
-//    }
-//    echo "排列共成功 $success 次，失败 $fail 次<br/>";
-//}
+        }
+    }
+}
