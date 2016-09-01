cf1c6c67647c168e8f64cee9af7cab5bca0614d3
pj919715177 <pengjun@ycf.com>
2016-09-01 08:46:19 +0800

    。。

--- DesignPattern/Single.php
+++ DesignPattern/Single.php
@@ -22,7 +22,7 @@ class Single
     {
         echo '
         我是单例模式，可以通过Factory生产对象，
-        因为我没有extra()函数,无法实例化Observerable接口，
+        因为我没有extra()函数,无法实现Observerable接口，
         只能通过Adapter，将Adapter实例放在Register的注册树属性数组中，
         将注册树的属性添加到Observer的观察者数组中，当Observer执行change()函数时，
         通过执行Adapter的update()函数，来执行我的update()函数
